52db2a87293efe9dc8e97a4b779ec3234c475a7d
zainabyousaf_ <91596027+zainab-yousaf@users.noreply.github.com>
2025-09-21 04:56:04 +0500

    file issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
     content: [
         "./pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -46,3 +47,5 @@ module.exports = {
     },
     plugins: [],
 };
+
+export default config;
